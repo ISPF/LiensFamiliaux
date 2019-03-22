b259f8e7f31e3ca436c866c909a9433bcf33002a
Laurent PELLET <laurentp@ispf.pf>
2019-03-22 12:49:41 -1000

    calcul de génération 1ere version

--- src/calculGeneration.R
+++ src/calculGeneration.R
@@ -1,3 +1,72 @@
 #calcul des générations
 
 load("copy.Rdata")
+
+
+getIdConcubin<-function(numeroIndividu)
+{
+  x <- unique(liens[IDTO %in% numeroIndividu & idLien %in% c(19,20), IDFROM])
+  if(length(x)>0)
+    x[[1]]
+  else
+    NA
+}
+
+getIdParent<-function(numeroIndividu)
+{
+  x <- unique(liens[IDTO %in% numeroIndividu & idLien %in% c(15,16), IDFROM])
+  if(length(x)>0)
+    x[[1]]
+  else
+    NA
+}
+
+getGenerationParent<-function(numeroIndividu)
+{
+  if (!is.na(getIdParent(numeroIndividu)))
+    famille[ID==getIdParent(numeroIndividu),GEN]
+  else 
+    NA
+}
+
+getGenerationConcubin<-function(numeroIndividu)
+{
+  if (!is.na(getIdConcubin(numeroIndividu)))
+    famille[ID==getIdConcubin(numeroIndividu),GEN]
+  else
+    NA
+}
+
+calculeGenerations<-function(verbose=FALSE)
+{
+  
+  #A ordonner pour le parcours de l'arbre
+  
+  famille[1,GEN:=1]
+  for (i in 2:famille[, .N]) {
+    i <- 5
+    id <- famille[i,ID]
+    #if (verbose==TRUE)
+    GENC <- getGenerationConcubin(id)
+    GENP  <- getGenerationParent(id)
+    cat(sprintf("Individus : %s\tGénération du concubin : %s et du parent : %s\n", id, GENC, GENP))
+    if (!is.na(GENP)){
+      famille[i, GEN:=GENP+1]
+    } else{
+      if (!is.na(GENC))
+        famille[i,GEN:=GENC]
+      else
+        famille[i, GEN:=0]
+    }
+  }
+}
+
+
+NUMF <- getNUMFAbyTaillMenage(14)
+
+famille <- nodes[NUMFA==NUMF, ]
+liens <- links[NUMFA==NUMF]
+famille[1,GEN:=1]
+calculeGenerations(verbose = F)
+plotFamily(NUMF)
+famille

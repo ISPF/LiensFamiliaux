14e3a8720f6d5123b51e27b80eac3741b1bfb5bc
Laurent PELLET <laurentp@ispf.pf>
2019-03-28 12:25:36 -1000

    Nouvelle version du main

--- Main.R
+++ Main.R
@@ -1,28 +1,18 @@
 source("src/Functions.R")
 
+#attention à déziper le fichier files.zip dans /input
+
 nodes <- fread("input/nodesRP.csv")
 links <- fread("input/linksRP.csv")
 sbGeneal <- fread("input/sbGenealRP.csv")
 
-save.image("copy.Rdata")
+#save.image("copy.Rdata")
 
-#listFamillesAObserver <- c(2,5,8,9,51,86,87,127,130,149,150,151)
-#listFamilles<-c(141702,141705,141715,141716,150185,150461,150463,150761,150772,150933,150951,150971)
-#NUMF <- 161592
-#EE NUMF <- 150185
-
-NUMF <- getNUMFAbyTaillMenage(14)
+#NUMF <- getNUMFAbyTaillMenage(14)
+NUMF <- 32347
 printNodes(NUMF)
 printLinks(NUMF)
-plotFamily(NUMF)
-
-plotFamily(getNUMFAbyTaillMenage(5))
-
-printNodes(NUMF)
-printLinks(NUMF)
-printAllDetail(NUMF)
-plotFamily(NUMF)
-plotGenealogy(NUMF, 7)
-plotGenealogy(NUMF, 5)
-plotFamilyPath(NUMF, 1,4)
-plotFamilyPath(NUMF, 1,5)
+plotPedigree(NUMF)
+#plotFamily(NUMF)
+#plotGenealogy(NUMF, 6)
+#plotFamilyPath(NUMF, 1,5)
